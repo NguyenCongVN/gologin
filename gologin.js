5d833c3ac5e8b97c20f7aba7a06047747bf87daa
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2022-10-06 12:29:48 +0700

    Update gologin.js

--- gologin.js
+++ gologin.js
@@ -387,8 +387,14 @@ class GoLogin {
     let profile;
     let profile_folder;
     // NOTICE: santize profile folder before remove because sometimes browser is terminated unexpectedly
-    await this.sanitizeProfile();
-    await this.getNewZipProfile();
+    try
+    {
+      await this.getNewZipProfile();
+    }
+    catch (e)
+    {
+      console.log("getNewZipProfile error", e);
+    }
     await rimraf(profilePath);
     debug("-", profilePath, "dropped");
 
